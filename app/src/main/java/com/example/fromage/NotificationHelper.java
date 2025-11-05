e7b199f318dd30cbe3a3cb4b04f3b3d0bdb2e85d
UneGrandChevr <nino.coillard77@gmail.com>
2025-11-05 11:52:35 +0100

    ajout de l'autorisation pour les notifs

--- app/src/main/java/com/example/fromage/NotificationHelper.java
+++ app/src/main/java/com/example/fromage/NotificationHelper.java
@@ -1,10 +1,13 @@
 package com.example.fromage;
 
+import android.Manifest;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Build;
 
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
@@ -28,13 +31,23 @@ public class NotificationHelper {
 
     public static void sendNotification(Context context, String title, String message) {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
-                .setSmallIcon(R.drawable.icon_comte) // ton icône
+                .setSmallIcon(R.drawable.icon_app) // icône de l'application
                 .setContentTitle(title)
                 .setContentText(message)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setAutoCancel(true);
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) { //Vérifier que l'application a les autorisations pour les notifications
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         notificationManager.notify((int) System.currentTimeMillis(), builder.build());
     }
 }
